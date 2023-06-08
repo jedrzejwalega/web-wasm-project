9be443b13c323eb0f1d5a2963ec0c13435c44155
jedrzejwalega <jedrzejwalega@gmail.com>
2023-06-08 23:31:46 +0200

    Created server obj containing server specific args

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(name = "server", about = "wasm project server")]
+struct Server {
+    #[clap(short = 'a', long = "addr", default_value = "::1")]
+    addr: String,
+    #[clap(short = 'p', long = "port", default_value = "8080")]
+    port: u16
+}
+
+#[tokio::main]
+async fn main() {
+    let serv = Server::parse();
 }
