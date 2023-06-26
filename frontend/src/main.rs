ead1c95136f718a77ba8bad028bb909820d687f9
jedrzejwalega <jedrzejwalega@gmail.com>
2023-06-27 00:53:07 +0200

    frontend html, log and routing setup

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,14 @@
+use yew::prelude::*;
+use yew_router::prelude::*;
+
+#[derive(Clone, Routable, PartialEq)]
+enum Route{
+    #[at("/")]
+    Home
+}
+
 fn main() {
-    println!("Hello, world!");
+    let log_config = wasm_logger::Config::new(log::Level::Trace);
+    wasm_logger::init(log_config);
+    console_error_panic_hook::set_once();
 }
