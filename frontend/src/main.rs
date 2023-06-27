c8faa724f609eb413e835aa17b06c789dc30dcac
jedrzejwalega <jedrzejwalega@gmail.com>
2023-06-28 00:06:18 +0200

    Frontend for route "/"

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -7,8 +7,25 @@ enum Route{
     Home
 }
 
+fn switch( available_routes:Route) -> Html {
+    match available_routes{
+        Route::Home => html!{ <h1>{ "Hello Frontend!"}</h1>}
+    }
+}
+
+#[function_component(App)]
+fn app() -> Html {
+    html!{
+        <BrowserRouter>
+            <Switch<Route> render={switch} />
+        </BrowserRouter>
+    }
+}
+
+
 fn main() {
     let log_config = wasm_logger::Config::new(log::Level::Trace);
     wasm_logger::init(log_config);
     console_error_panic_hook::set_once();
+    yew::Renderer::<App>::new().render();
 }
