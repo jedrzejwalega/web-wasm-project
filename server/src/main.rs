d3d0b29a34f41ea4574d4685b233334327724b78
jedrzejwalega <jedrzejwalega@gmail.com>
2023-07-05 03:06:00 +0200

    Connecting server with fronend

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,7 +3,10 @@ use clap::Parser;
 use axum::{Router, routing::get, response::IntoResponse};
 use hyper::server::{Builder, conn::AddrIncoming};
 use tower_http::trace::TraceLayer;
-use tower::ServiceBuilder;
+use axum::body::{boxed, Body};
+use axum::http::{Response, StatusCode};
+use tower::{ServiceBuilder, ServiceExt};
+use tower_http::services::ServeDir;
 
 #[derive(Parser, Debug)]
 #[clap(name = "server", about = "wasm project server")]
@@ -13,7 +16,9 @@ struct Serv {
     #[clap(short = 'p', long = "port", default_value = "8080")]
     port: u16,
     #[clap(short = 'l', long = "log",default_value = "debug")]
-    log_level: String
+    log_level: String,
+    #[clap(short = 's', long= "static-directory", default_value = "./dist")]
+    static_dir: String
 }
 
 // development only function
@@ -27,8 +32,16 @@ async fn main() {
     let logging_layer = ServiceBuilder::new()
                         .layer(TraceLayer::new_for_http());
     let app: Router = Router::new()
-                                .route("/", get(root))
-                                .layer(logging_layer);
+                                .route("/api/hello", get(root))
+                                .fallback_service(get(|req| async move {
+    match ServeDir::new(server_parameters.static_dir).oneshot(req).await {
+        Ok(res) => res.map(boxed),
+        Err(err) => Response::builder()
+                    .status(StatusCode::INTERNAL_SERVER_ERROR)
+                    .body(boxed(Body::from(format!("error: {err}"))))
+                    .expect("error response"),
+        }}))
+        .layer(logging_layer);
     let localhost_address:IpAddr = IpAddr::V6(Ipv6Addr::LOCALHOST);
     let server_address:IpAddr = IpAddr::from_str(server_parameters.addr.as_str())
                                                 .unwrap_or(localhost_address);
