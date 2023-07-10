fb3de00cea92b2abbd0f3e3fe8de5de68e69dad6
jedrzejwalega <jedrzejwalega@gmail.com>
2023-07-10 02:16:21 +0200

    Frontend for hello route

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,5 +1,7 @@
 use yew::prelude::*;
 use yew_router::prelude::*;
+use wasm_bindgen_futures::spawn_local;
+use gloo_net::http::Request;
 
 #[derive(Clone, Routable, PartialEq)]
 enum Route{
@@ -22,6 +24,53 @@ fn app() -> Html {
         <BrowserRouter>
             <Switch<Route> render={switch} />
         </BrowserRouter>
+    }
+}
+
+#[function_component(HelloServer)]
+fn hello_server() -> Html {
+    let data = use_state(|| None);
+    {
+        let data = data.clone();
+        use_effect(move || {
+            if data.is_none() {
+                spawn_local(async move {
+                    let resp = Request::get("/api/hello").send().await.unwrap();
+                    let result = {
+                        if !resp.ok() {
+                            Err(format!(
+                                "Error fetching data {} ({})",
+                                resp.status(),
+                                resp.status_text()
+                            ))
+                        } else {
+                            resp.text().await.map_err(|err| err.to_string())
+                        }
+                    };
+                    data.set(Some(result));
+                });
+            }
+
+            || {}
+        });
+    }
+
+    match data.as_ref() {
+        None => {
+            html! {
+                <div>{"No server response"}</div>
+            }
+        }
+        Some(Ok(data)) => {
+            html! {
+                <div>{"Got server response: "}{data}</div>
+            }
+        }
+        Some(Err(err)) => {
+            html! {
+                <div>{"Error requesting data from server: "}{err}</div>
+            }
+        }
     }
 }
 
